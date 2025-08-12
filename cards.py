2de52899758a74f47e6712a286e4efb925a8438f
Denis <faceitlolaw@gmail.com>
2025-08-12 12:37:26 +0300

    added one-hot function that works

--- cards.py
+++ cards.py
@@ -3,35 +3,58 @@ from treys import Card, Deck, Evaluator
 
 class Draw():
     def __init__(self):
-        self.deck = Deck()
+
+        self.deck = Deck() # подтягиваем классы
         self.evaluator = Evaluator()
 
-    def draw_pocket_cards(self):
+        self.board = [] # инициализируем борд и руки, чтобы не было ошибки при вызове борда до его раздачи
+        self.player_hand = []
+        self.villain_hand = []
+
+    def draw_pocket_cards(self): # генерирует стартовые руки (оппа и игрока)
         self.player_hand = self.deck.draw(2)
         self.villain_hand = self.deck.draw(2)
         return self.player_hand, self.villain_hand
     
-    def draw_flop(self):
-        self.board = self.deck.draw(3)
+    def draw_flop(self): # раздает флоп
+        self.board.extend(self.deck.draw(3))
         return self.board
     
-    def draw_turn(self):
-        self.board = self.deck.draw(1)
+    def draw_turn(self):# раздает терн
+        self.board.extend(self.deck.draw(1))
         return self.board
     
-    def draw_river(self):
-        self.board = self.deck.draw(1)
+    def draw_river(self):# раздает ривер
+        self.board.extend(self.deck.draw(1))
         return self.board
     
-    def get_pretty_cards(self):
+    def get_pretty_cards(self): # чисто для вывода
         self.player_str = "Player hand: " + " ".join([Card.int_to_str(c) for c in self.player_hand])
         self.villain_str = "Villain hand: " + " ".join([Card.int_to_str(c) for c in self.villain_hand])
-        self.board_str = "Board: " + " ".join([Card.int_to_str(c) for c in self.board])
+        self.board_str = "Board: " + " ".join([Card.int_to_str(c) for c in self.board]) 
         return self.player_str, self.villain_str, self.board_str
     
-    def get_score(self):
+    def get_score(self): # по этой херне будем разбирать кто выиграл
         self.player_score = self.evaluator.evaluate(self.board, self.player_hand)
         self.villain_score = self.evaluator.evaluate(self.board, self.villain_hand)
+        return self.player_score, self.villain_score
+    
+    def card_to_index(self, cards): # переводит в число (0 - 51)
+        index_list = []
+        for card in cards:
+            rank = Card.get_rank_int(card)
+            suit = Card.get_suit_int(card)
+            index = (rank - 2) * 4 + suit
+            index_list.append(index)
+        return index_list
+    
+    def cards_to_one_hot(self, cards): # ванхот
+        one_hot_vector = torch.zeros(52)
+        index_list = self.card_to_index(cards)
+        for index in index_list:
+            one_hot_vector[index] = 1
+        return one_hot_vector
+
 
 
 
--- env.py
+++ env.py
@@ -3,6 +3,7 @@
 import random
 import torch
 from cards import Draw
+cards = Draw() # подтягиваем класс
 class PokerEnv:
     def __init__(self): # конструктор
         self.reward = 0 # задаем значение награды (оно будет доступно в любом методе этого класса)
@@ -10,33 +11,60 @@ class PokerEnv:
         self.reset()
 
     def reset(self): # сбрасываем стек и все остальное
-        self.hand = random.randint(0,168) # выбираем случайную комбинацию из двух карт (0-168) чтоб было удобнее в тензор ведь там от 0
+        self.player_hand = cards.draw_pocket_cards()[0]
+        self.villain_hand = cards.draw_pocket_cards()[1]
         self.done = False
+        return self.player_hand, self.villain_hand
 
     def get_hand(self): # вытаскиваем комбинацию
         return self.hand # возвращаем комбинацию свою
 
-    def get_one_hot_vector(self): # просто hand, потому что описание через self дает возможность вызывать эту функцию из любого метода класса
-        self.one_hot = torch.zeros(169)
-        self.one_hot[self.hand] = 1
-        return self.one_hot
+    def to_one_hot(self,card): # сюда подается целая комба (например стартовая рука или борд)
+        one_hot_vector = cards.cards_to_one_hot(card) # вернет ретерном ванхот вектор
+        return one_hot_vector
+    
 
 
-    def step(self,action): # действие (пуш или фолд) пуш = 1 фолд = 0
-        if action == 0: # фолд
-            reward = -0.5 # штраф за фолд (не надо это здесь суммировать, это локально)
-            self.done = True # и игра закончена
-            self.opp_hand = None
-            return reward, self.done, self.hand, self.opp_hand # возвращаем награду и done
+    # def step(self,action): # действие (пуш или фолд) пуш = 1 фолд = 0
+    #     if action == 0: # фолд
+    #         reward = -0.5 # штраф за фолд (не надо это здесь суммировать, это локально)
+    #         self.done = True # и игра закончена
+    #         self.opp_hand = None
+    #         return reward, self.done, self.hand, self.opp_hand # возвращаем награду и done
 
-        else: # если пуш
-            self.opp_hand = random.randint(0,168) # выбираем случайную комбинацию из двух карт, делаем это тут чтобы заранее не знать комбинацию противника
-            if self.hand > self.opp_hand: # если наша комбинация лучше то мы выигрываем
-                reward = 10 # выигрыш
+    #     else: # если пуш
+    #         self.opp_hand = random.randint(0,168) # выбираем случайную комбинацию из двух карт, делаем это тут чтобы заранее не знать комбинацию противника
+    #         if self.hand > self.opp_hand: # если наша комбинация лучше то мы выигрываем
+    #             reward = 10 # выигрыш
                 
-            else: # если наша комбинация хуже то мы проигрываем
-                reward = -10 # проигрыш
-            self.done = True # и игра закончена
-            return reward, self.done, self.hand, self.opp_hand # возвращаем награду и done
+    #         else: # если наша комбинация хуже то мы проигрываем
+    #             reward = -10 # проигрыш
+    #         self.done = True # и игра закончена
+    #         return reward, self.done, self.hand, self.opp_hand # возвращаем награду и done
 
 env = PokerEnv()
+draw = env.reset()
+player_hand = draw[0]
+villain_hand = draw[1]
+player_hand_one_hot = cards.cards_to_one_hot(player_hand) # one hot вектор для своей руки
+villain_hand_one_hot = cards.cards_to_one_hot(villain_hand) # one hot вектор для руки противника
+
+flop = cards.draw_flop() # заранее раздаем, но не даем эту инфу модели
+turn = cards.draw_turn() # заранее раздаем, но не даем эту инфу модели
+river = cards.draw_river() # заранее раздаем, но не даем эту инфу модели
+
+flop_one_hot = cards.cards_to_one_hot(flop) # one hot вектор для флопа
+turn_one_hot = cards.cards_to_one_hot(turn) # one hot вектор для терна
+river_one_hot = cards.cards_to_one_hot(river) # one hot вектор для ривера
+
+pretty_cards = cards.get_pretty_cards() # вытщаит все карты в красивом виде (борд тоже)
+print(pretty_cards)
+
+
+
+
+
+
+
+
+
